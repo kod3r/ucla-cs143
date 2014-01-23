b2679e04eb899b56fa41936b5420cef2159d2904
Ivan Petkov <ivanppetkov@gmail.com>
2014-01-22 16:42:45 -0800

    Add non-null constraints to the following tables:
    Movie.id
    Movie.title
    Actor.id
    Director.id
    MovieDirector.mid
    MovieDirector.did
    MovieActor.mid
    MovieActor.aid
    MovieActor.role
    Review
    Review.name
    Review.time
    Review.mid
    MaxPersonID.id
    MaxMovieID.id
    
    @pcrumm please review

--- project-1b/create.sql
+++ project-1b/create.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `Movie` (
-	`id` INT,
-	`title` VARCHAR(100),
+	`id` INT NOT NULL,
+	`title` VARCHAR(100) NOT NULL,
 	`year` INT,
 	`rating` VARCHAR(10),
 	`company` VARCHAR(50)
 );
 
 CREATE TABLE `Actor` (
-	`id` INT,
+	`id` INT NOT NULL,
 	`last` VARCHAR(20),
 	`first` VARCHAR(20),
 	`sex` VARCHAR(6),
@@ -16,7 +16,7 @@ CREATE TABLE `Actor` (
 );
 
 CREATE TABLE `Director` (
-	`id` INT,
+	`id` INT NOT NULL,
 	`last` VARCHAR(20),
 	`first` VARCHAR(20),
 	`dob` DATE,
@@ -24,33 +24,33 @@ CREATE TABLE `Director` (
 );
 
 CREATE TABLE `MovieGenre` (
-	`mid` INT,
-	`genre` VARCHAR(20)
+	`mid` INT NOT NULL,
+	`genre` VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE `MovieDirector` (
-	`mid` INT,
-	`aid` INT
+	`mid` INT NOT NULL,
+	`did` INT NOT NULL
 );
 
 CREATE TABLE `MovieActor` (
-	`mid` INT,
-	`aid` INT,
-	`role` VARCHAR(50)
+	`mid` INT NOT NULL,
+	`aid` INT NOT NULL,
+	`role` VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE `Review` (
-	`name` VARCHAR(20),
-	`time` TIMESTAMP,
-	`mid` INT,
-	`rating` INT,
+	`name` VARCHAR(20) NOT NULL,
+	`time` TIMESTAMP NOT NULL,
+	`mid` INT NOT NULL,
+	`rating` INT NOT NULL,
 	`comment` VARCHAR(500)
 );
 
 CREATE TABLE `MaxPersonID` (
-	`id` INT
+	`id` INT NOT NULL
 );
 
 CREATE TABLE `MaxMovieID` (
-	`id` INT
+	`id` INT NOT NULL
 );
