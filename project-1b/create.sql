75458825bb92f8493cbf2327e394cf0dd41d3933
Ivan Petkov <ivanppetkov@gmail.com>
2014-01-22 19:53:57 -0800

    Add primary and foreign keys, along with unique constraints
    * Also set the engine to INNODB on all tables for foreign key references

--- project-1b/create.sql
+++ project-1b/create.sql
@@ -3,8 +3,9 @@ CREATE TABLE `Movie` (
 	`title` VARCHAR(100) NOT NULL,
 	`year` INT,
 	`rating` VARCHAR(10),
-	`company` VARCHAR(50)
-);
+	`company` VARCHAR(50),
+	PRIMARY KEY(`id`)
+) ENGINE = INNODB;
 
 CREATE TABLE `Actor` (
 	`id` INT NOT NULL,
@@ -12,45 +13,57 @@ CREATE TABLE `Actor` (
 	`first` VARCHAR(20),
 	`sex` VARCHAR(6),
 	`dob` DATE,
-	`dod` DATE
-);
+	`dod` DATE,
+	PRIMARY KEY(`id`)
+) ENGINE = INNODB;
 
 CREATE TABLE `Director` (
 	`id` INT NOT NULL,
 	`last` VARCHAR(20),
 	`first` VARCHAR(20),
 	`dob` DATE,
-	`dod` DATE	
-);
+	`dod` DATE,
+	PRIMARY KEY(`id`)
+) ENGINE = INNODB;
 
 CREATE TABLE `MovieGenre` (
 	`mid` INT NOT NULL,
-	`genre` VARCHAR(20) NOT NULL
-);
+	`genre` VARCHAR(20) NOT NULL,
+	UNIQUE(`mid`, `genre`),
+	FOREIGN KEY(`mid`) references Movie(`id`)
+) ENGINE = INNODB;
 
 CREATE TABLE `MovieDirector` (
 	`mid` INT NOT NULL,
-	`did` INT NOT NULL
-);
+	`did` INT NOT NULL,
+	UNIQUE(`mid`, `did`),
+	FOREIGN KEY(`mid`) references Movie(`id`),
+	FOREIGN KEY(`did`) references Director(`id`)
+) ENGINE = INNODB;
 
 CREATE TABLE `MovieActor` (
 	`mid` INT NOT NULL,
 	`aid` INT NOT NULL,
-	`role` VARCHAR(50) NOT NULL
-);
+	`role` VARCHAR(50) NOT NULL,
+	UNIQUE(`mid`, `aid`, `role`),
+	FOREIGN KEY(`mid`) references Movie(`id`),
+	FOREIGN KEY(`aid`) references Actor(`id`)
+) ENGINE = INNODB;
 
 CREATE TABLE `Review` (
 	`name` VARCHAR(20) NOT NULL,
 	`time` TIMESTAMP NOT NULL,
 	`mid` INT NOT NULL,
 	`rating` INT NOT NULL,
-	`comment` VARCHAR(500)
-);
+	`comment` VARCHAR(500),
+	UNIQUE(`name`, `mid`),
+	FOREIGN KEY(`mid`) references Movie(`id`)
+) ENGINE = INNODB;
 
 CREATE TABLE `MaxPersonID` (
 	`id` INT NOT NULL
-);
+) ENGINE = INNODB;
 
 CREATE TABLE `MaxMovieID` (
 	`id` INT NOT NULL
-);
+) ENGINE = INNODB;
