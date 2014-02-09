45f74bcd91f76043914830a75d2f052cdfd0295b
Phil Crumm <pcrumm@p3net.net>
2014-02-08 18:04:30 -0800

    Index page.

---
+++ project-1c/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once('common.php');
+page_header( 'Index' );
+?>
+<p>Welcome to the Phivan movie and actor database. To begin, please select an option above.</p>
+<?php page_footer(); ?>
