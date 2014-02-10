ffd7747bed503a9a38d51b39b07fa4966cda25d7
Ivan Petkov <ivanppetkov@gmail.com>
2014-02-09 21:16:22 -0800

    Review: list rating options from greatest to least instead of reverse

--- project-1c/review.php
+++ project-1c/review.php
@@ -65,12 +65,12 @@ page_header( 'New Review' );
 			Rating:
 			<select name="review[rating]">
 				<option></option>
-				<option value='0'>0</option>
-				<option value='1'>1</option>
-				<option value='2'>2</option>
-				<option value='3'>3</option>
-				<option value='4'>4</option>
 				<option value='5'>5</option>
+				<option value='4'>4</option>
+				<option value='3'>3</option>
+				<option value='2'>2</option>
+				<option value='1'>1</option>
+				<option value='0'>0</option>
 			</select>
 			<br>
 			Comments:
